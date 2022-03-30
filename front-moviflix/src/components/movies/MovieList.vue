2cc92e20c0c2fc3f465668d266d8838a76a2695e
louis <louis.chomel@gmail.com>
2022-03-30 15:50:16 +0200

    Fetch liste film simple

---
+++ front-moviflix/src/components/movies/MovieList.vue
@@ -0,0 +1,79 @@
+<template>
+    <h1>
+        Liste des Films 
+    </h1>
+    <div class="post">
+        <div v-if="loading">Loading...</div>
+
+        <div v-if="error">{{ error }}</div>
+
+        <div v-if="movieList" >
+         <h2>{{ movieList[0].title }}</h2>
+        </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  data() {
+    return {
+      loading: false,
+      movieList: null,
+      error: null,
+    }
+  },
+  created() {
+    // watch the params of the route to fetch the data again
+    this.$watch(
+      () => this.$route.params,
+      () => {
+        this.fetchData()
+      },
+      // fetch the data when the view is created and the data is
+      // already being observed
+      { immediate: true }
+    )
+  },
+  methods: {
+    fetchData: async function(){
+      this.error = this.movieList = null
+      this.loading = true
+      // replace `getPost` with your data fetching util / API wrapper
+      
+      fetch('http://localhost:3002/API/v1/movies')
+        .then(response => {
+            if (response.ok){
+                return response.json() 
+            }      
+            throw new Error('Erreur lors de la connection à l API')
+        })
+        .then(responseJSON =>{
+            this.movieList = responseJSON
+            this.loading = false
+        })
+        .catch((error)=>{
+            this.error = error.toString()
+            console.log(error)
+        })
+    
+    //   getPost(this.$route.params.id, (err, post) => {
+    //     this.loading = false
+    //     if (err) {
+    //       this.error = err.toString()
+    //     } else {
+    //       this.post = post
+    //     }
+    //   })
+    },
+  },
+})
+export default class Movie extends Vue {
+  msg!: string
+}
+</script>
+
+
+ 
