437bae1bfbd63ff79850e0e77db35e34f9c61f46
Mathieu Lapassade <mathieu.lapassade@eisti.fr>
2022-03-31 12:06:04 +0200

    Ajout des logs Fix de routes utilisant les MongoID

--- back-moviflix/db-API/app/routers/movies.py
+++ back-moviflix/db-API/app/routers/movies.py
@@ -1,9 +1,8 @@
-from inspect import Parameter
 from fastapi import APIRouter, Depends
 from ..database.connection import get_database
 from ..crud.movies import fetch_movie_by_id,add_movie,fetch_movies_with_projection,update_movie
 from ..models.movie import Movie,MovieIns,UpdatedMovie
-import logging
+from ..models.common import PyObjectId
 
 from typing import List
 from fastapi import HTTPException, Body, status
@@ -11,22 +10,9 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
 router = APIRouter(
-     prefix="/movies",
+    prefix="/movies",
     tags=["movies"],
 )
-
-@router.on_event("startup")
-async def startup_event():
-    loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
-    print("Available loggers in movies: ")
-    print(loggers)
-
-    for logg in loggers:
-        print(f"{logg} : {logg.handlers}")
-    
-    root_logger = logging.getLogger()
-    print("rootloggershandlers")
-    print(root_logger.handlers)
 
 @router.get("/",response_description="List all movies in DB, with projection. If you don't provide any projection field, uses title and movieCoverUrl") #response_model=List[Movie]
 async def get_movies_with_projection(limit:int = 20, page:int = 0, parameters:str = None, db = Depends(get_database)):
@@ -47,8 +33,15 @@ async def get_movies_with_projection(limit:int = 20, page:int = 0, parameters:st
     return JSONResponse(content=json_compatible_item_data)
 
 @router.get("/{movieId}",response_description="Find a single movie with its MongoDB ID",response_model=Movie)
-async def get_movie(movieId : str, db = Depends(get_database)): 
-    movie = await fetch_movie_by_id(db,movieId)
+async def get_movie(movieId : str, db = Depends(get_database)):
+    # Check if the movie ID is valid
+    try:
+        movieIdToFetch = PyObjectId(movieId)
+    except:
+        raise HTTPException(status_code=status.HTTP_418_IM_A_TEAPOT, detail=f"The provided user ID '{movieId}' is not a valid Mongo ID")
+
+    movie = await fetch_movie_by_id(db,movieIdToFetch)
+
     if movie is not None : 
         return movie
 
